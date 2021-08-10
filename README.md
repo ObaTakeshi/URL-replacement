fffd7bae5413e5ee5d2740eafa653d6a7d71508b
ObaTakeshi <t1233banana@yahoo.co.jp>
2021-08-10 12:51:57 +0900

    jsonファイルに変更

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # URL replacement
-URLのパスをそのままにホストを置換したりできるAlfred Workflowです。
+
+URL のパスをそのままにホストを置換したりできる Alfred Workflow
+
+## url_list ファイル
+
+`~/Documents/URL_replacement/url_list.json`
+
+上のパスに以下の内容の json ファイルを配置。
+
+動作として、それぞれの置換候補を探索し、置換候補内に現在の URL がヒットした場合、その置換候補のブロックを Alfred に返す。
+
+```
+{
+    "data": [
+        // 置換候補1
+        [
+            {
+                "name": ページ名,
+                "url": ページのURL
+            },
+            {
+                "name": ページ名,
+                "url": ページのURL
+            }
+        ],
+
+        // 置換候補2
+        [
+            {
+                "name": ページ名,
+                "url": ページのURL
+            },
+            {
+                "name": ページ名,
+                "url": ページのURL
+            }
+        ]
+    ]
+}
+```
--- lib/lib.py
+++ lib/lib.py
@@ -1,19 +1,16 @@
-import csv
+import json
+
 
 class URL_lib:
     @staticmethod
-    def hoge():
-        return 0
-
-    @staticmethod
     def search_url_list(current_url_host, url_list_file):
-        with open(url_list_file, 'r') as csvfile:
-            url_list = csv.reader(csvfile, skipinitialspace=True)
+        with open(url_list_file, 'r') as jsonfile:
+            url_list = json.load(jsonfile)
 
-            for row in url_list:
-                for col in row:
-                    if current_url_host in col:
-                        return row
+        for url_blocks in url_list['data']:
+            for url_block in url_blocks:
+                if current_url_host in url_block['url']:
+                    return url_blocks
 
         return 0
 
@@ -24,5 +21,3 @@ class URL_lib:
     @staticmethod
     def get_url_path(url):
         return '/'.join(url.split('/')[3:])
-
-
